416a7c461ab6aa5455527a2440bd3bd91e139376
Marc Weistroff <marc@weistroff.net>
2017-12-21 11:46:40 +0100

    Enable autowire for ImageServerBundle

--- src/AudienceHero/Bundle/ImageServerBundle/DependencyInjection/AudienceHeroImageServerExtension.php
+++ src/AudienceHero/Bundle/ImageServerBundle/DependencyInjection/AudienceHeroImageServerExtension.php
@@ -3,9 +3,12 @@
 namespace AudienceHero\Bundle\ImageServerBundle\DependencyInjection;
 
 use AudienceHero\Bundle\ImageServerBundle\DependencyInjection\Compiler\ChainTransformerCompilerPass;
+use AudienceHero\Bundle\ImageServerBundle\Server\Server;
+use AudienceHero\Bundle\ImageServerBundle\Server\ServerInterface;
 use AudienceHero\Bundle\ImageServerBundle\Transformer\TransformerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Intl\DateFormatter\DateFormat\Transformer;
@@ -24,6 +27,11 @@ class AudienceHeroImageServerExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $container->setAlias(ServerInterface::class, Server::class);
 
         $container->setParameter('audiencehero_image_server.doctrine_cache', $config['cache']);
         $container->registerForAutoconfiguration(TransformerInterface::class)
